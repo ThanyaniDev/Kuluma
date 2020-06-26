f59ede9304cb4aadd1412767f150b581b8f86cde
ThanyaniTshikonwani <thanyani.tshikonwani@umuzi.org>
2020-06-26 17:03:26 +0200

    Initial Commit

---
+++ Kuluma/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kuluma
+//
+//  Created by Thanyani on 2020/06/26.
+//  Copyright © 2020 Thanyani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
